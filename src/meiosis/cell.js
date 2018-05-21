2853d90b4efc64e35541f892a7b6bde2f4d98fee
Tommydreamer57 <minilao94@yahoo.com>
2018-05-21 09:15:30 -0600

    first commit

---
+++ src/meiosis/cell.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function create(update) {
+    // INITIAL DATA
+    // CHILDREN
+    // FUNCTIONS
+    // COMPONENT
+    return {
+        view(model) {
+            return (
+                <div>
+
+                </div>
+            );
+        }
+    };
+}
